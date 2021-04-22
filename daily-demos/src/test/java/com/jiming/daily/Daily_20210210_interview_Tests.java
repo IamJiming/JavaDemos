84db1371781f04964ea2d48095122ca57be16ddd
tianjiming <tianjiming@chinapost.com.cn>
2021-04-22 09:08:28 +0800

    daily 创建demo

---
+++ daily-demos/src/test/java/com/jiming/daily/Daily_20210210_interview_Tests.java
@@ -0,0 +1,19 @@
+package com.jiming.daily;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Java面试题
+ *
+ * @author Mr.tjm
+ * @date 2021-2-10 17:25
+ */
+public class Daily_20210210_interview_Tests {
+    /**
+     *
+     */
+    @Test
+    void miss_test_1() {
+
+    }
+}
